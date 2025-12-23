3c7321f2e81f488824b0bf16a324462e09258fd6
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2025-12-23 12:46:48 -0800

    Time: 0 ms (100%), Space: 47.6 MB (56.29%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int trap(int[] height) {
+        int l = 0;
+        int r = height.length - 1;
+        int lmax = Integer.MIN_VALUE;
+        int rmax = Integer.MIN_VALUE;
+        int q = 0;
+
+        while (l < r) {
+            if (height[l] < height[r]) {
+                if (height[l] >= lmax) lmax = height[l];
+                else q += lmax - height[l];
+                l++;
+            } else {
+                if (height[r] >= rmax) rmax = height[r];
+                else q += rmax - height[r];
+                r--;
+            } // if-else
+        } // while
+
+        return q;
+    } // trap
+} // Solution
