8f068a83216f6e490c31222cca566b198f786834
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2025-12-23 20:01:22 -0800

    Time: 3 ms (86.48%), Space: 52.5 MB (28.01%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int cnt = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    bfs(grid, i, j);
+                    cnt++;
+                } // if
+            } // for
+        } // for
+
+        return cnt;
+    } // numIslands
+
+    private void bfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
+
+        grid[i][j] = '#';
+
+        bfs(grid, i + 1, j);
+        bfs(grid, i - 1, j);
+        bfs(grid, i, j + 1);
+        bfs(grid, i, j - 1);
+    } // bfs
+} // Solution
