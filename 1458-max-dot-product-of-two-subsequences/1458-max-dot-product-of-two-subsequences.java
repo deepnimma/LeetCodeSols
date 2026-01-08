b2e90307e9295c83264bb671de066be81cccc3c5
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-01-08 15:10:07 -0500

    Time: 1070 ms (5.3%), Space: 46.4 MB (65.91%) - LeetHub

---
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.java
@@ -0,0 +1,48 @@
+class Solution {
+    int[][] memo;
+
+    public int maxDotProduct(int[] nums1, int[] nums2) {
+        int firstMax = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        int firstMin = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        for (int num : nums1) {
+            firstMax = Math.max(firstMax, num);
+            firstMin = Math.min(firstMin, num);
+        } // for
+
+        for (int num : nums2) {
+            secondMax = Math.max(secondMax, num);
+            secondMin = Math.min(secondMin, num);
+        } // for
+
+        if (firstMax < 0 && secondMin > 0) return firstMax * secondMin;
+        if (firstMin > 0 && secondMax < 0) return firstMin * secondMax;
+
+        memo = new int[nums1.length][nums2.length];
+        return dp(0, 0, nums1, nums2);
+    } // maxDotProduct
+
+    private int dp(int i, int j, int[] nums1, int[] nums2) {
+        if (i == nums1.length || j == nums2.length) return 0;
+        if (memo[i][j] != 0) return memo[i][j];
+
+        int use = nums1[i] * nums2[j] + dp(i + 1, j + 1, nums1, nums2);
+        memo[i][j] = Math.max(use, Math.max(dp(i + 1, j, nums1, nums2), dp(i, j + 1, nums1, nums2)));
+        // printMatrix(memo);
+        return memo[i][j];
+    } // dp
+
+    private void printMatrix(int[][] memo) {
+        for (int i = 0; i < memo.length; i++) {
+            for (int j = 0; j < memo[0].length; j++) {
+                System.out.print(memo[i][j] + " ");
+            } // for
+
+            System.out.println();
+        } // for
+
+        System.out.println();
+    } // printMatrix
+} // Solution
