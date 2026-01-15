19deb5f3687a8befd5a14c4093c3a5579b5a12e5
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-01-14 19:31:53 -0500

    Time: 8 ms (71.58%), Space: 65.4 MB (29.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -5,14 +5,13 @@ class Solution {
         return res;
     } // partition
 
-    private void dfs(int st, List<List<String>> res, List<String> curr, String s) {
-        if (st >= s.length()) res.add(new ArrayList<>(curr));
+    private void dfs(int start, List<List<String>> res, List<String> curr, String s) {
+        if (start >= s.length()) res.add(new ArrayList<>(curr));
 
-        for (int e = st; e < s.length(); e++) {
-            if (isPalindrome(s, st, e)) {
-                curr.add(s.substring(st, e + 1));
-
-                dfs(e + 1, res, curr, s);
+        for (int end = start; end < s.length(); end++) {
+            if (isPalindrome(s, start, end)) {
+                curr.add(s.substring(start, end + 1));
+                dfs(end + 1, res, curr, s);
                 curr.remove(curr.size() - 1);
             } // if
         } // for
