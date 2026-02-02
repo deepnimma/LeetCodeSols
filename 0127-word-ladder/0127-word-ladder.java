0d25071f8ecf0b0e9884fb715e8ad34ca8342a0d
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-02-02 13:03:46 -0500

    Time: 443 ms (14.37%), Space: 63.9 MB (5.01%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Map<String, List<String>> data = new HashMap<>();
+        preprocess(wordList, data);
+        Queue<Pair<String, Integer>> q = new LinkedList<>();
+        HashSet<String> visited = new HashSet<>();
+        q.offer(new Pair<>(beginWord, 1));
+
+        while (!q.isEmpty()) {
+            Pair<String, Integer> curr = q.poll();
+            if (visited.contains(curr.getKey())) continue;
+            else visited.add(curr.getKey());
+            if (curr.getKey().equals(endWord)) return curr.getValue();
+            List<String> wcs = getWildcards(curr.getKey());
+
+            for (String wc : wcs) {
+                System.out.println(wc);
+                List<String> valids = data.get(wc);
+                if (valids == null) continue;
+                for (String v : valids) {
+                    q.offer(new Pair<>(v, curr.getValue() + 1));
+                } // for
+            } // for
+        } // while
+
+        return 0;
+    } // ladderLength
+
+    private List<String> getWildcards(String word) {
+        List<String> ans = new ArrayList<>();
+        for (int i = 0; i < word.length(); i++) {
+            String ab = word.substring(0, i) + "*" + word.substring(i + 1, word.length());
+            ans.add(ab);
+        } // for
+        return ans;
+    } // getWildCards
+
+    private Map<String, List<String>> preprocess(List<String> wordList, Map<String, List<String>> data) {
+        for (String word : wordList) {
+            List<String> wcs = getWildcards(word);
+            for (String ab : wcs) {
+                if (!data.containsKey(ab)) data.put(ab, new ArrayList<>());
+                System.out.println(ab);
+                data.get(ab).add(word);
+            } // for
+        } // for
+
+        return data;
+    } // preprocess
+} // Solution
