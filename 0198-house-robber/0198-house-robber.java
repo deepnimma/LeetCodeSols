695a5324c66806e3c5199cd343bf00a0cf828ba1
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-02-04 11:24:29 -0500

    Time: 0 ms (100%), Space: 43.2 MB (14.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) return nums[0];
-        if (nums.length == 2) return Math.max(nums[0], nums[1]);
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        int[] ans = new int[nums.length];
-        ans[0] = nums[0];
-        ans[1] = Math.max(nums[0], nums[1]);
-
-        for (int i = 2; i < nums.length; i++) {
-            ans[i] = Math.max(ans[i - 2] + nums[i], ans[i - 1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
         } // for
 
-        return ans[nums.length - 1];
+        return dp[n - 1];
     } // rob
 } // Solution
