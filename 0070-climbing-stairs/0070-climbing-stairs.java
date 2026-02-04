e5dc99071dc88e47fccd035bcae9b0f9f395be1e
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-02-04 11:14:43 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n];
         dp[0] = 1;
-        dp[1] = 1;
+        dp[1] = 2;
 
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i < n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         } // for
 
-        return dp[n];
+        return dp[n - 1];
     } // climbStairs
-} // Solutions
+} // Solution
