cd364b95103287a79461024e554f32fe5fb52430
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-06-05 21:55:54 -0700

    Time: 4 ms (20.07%), Space: 44.3 MB (7.08%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        String ans = new String();
+        int i = 0, j = 0;
+        int n = word1.length(), m = word2.length();
+
+        while (i < n && j < m) {
+            if (ans.length() % 2 == 0) ans += word1.charAt(i++);
+            else ans += word2.charAt(j++);
+        } // while
+
+        while (i < n) {
+            ans += word1.charAt(i++);
+        } // while
+
+        while (j < m) {
+            ans += word2.charAt(j++);
+        } // while
+
+        return ans;
+    } // mergeAlternatively
+} // Solution
