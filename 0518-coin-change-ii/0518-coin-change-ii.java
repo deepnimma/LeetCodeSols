10c969cf72e1b877a0f30c974b023821023f9255
Deepesh Nimma <56356841+deepnimma@users.noreply.github.com>
2026-06-06 20:54:09 -0700

    Time: 12 ms (47.34%), Space: 56.6 MB (47.52%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -7,7 +7,10 @@ class Solution {
             dp[i][0] = 1;
 
             for (int j = 1; j <= amount; j++) {
-                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+                dp[i][j] = dp[i - 1][j];
+                if (j >= coins[i - 1]) {
+                    dp[i][j] += dp[i][j - coins[i - 1]];
+                } // if
             } // for
 
             // printArr(dp);
